7fc21042e2d9720f16e8cc99a38bd4da4e7b6897
damar <damar@gitlab.lab>
2022-07-18 18:08:56 +0700

    mobile first

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -9,8 +9,8 @@ const Header = () => {
                     <div className="container w-10/12 mx-auto">
                         <img src={'/pixel_me.png'} alt="logo" className="w-40 h-40" />
                         {/* <Image src="/pixel_me.png" width={128} height={128} /> */}
-                        <p className="text-4xl font-semibold">Damar Krisnandi Ramdan</p>
-                        <p className="text-xl">Frontend Developer</p>
+                        <p className="text-2xl md:text-4xl font-semibold">Damar Krisnandi Ramdan</p>
+                        <p className="text-lg md:text-xl">Frontend Developer</p>
                     </div>
                 </div>
             </div>
